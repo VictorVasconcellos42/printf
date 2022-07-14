1ac45280c46fbb8deb84ebe273436de85a678f08
vde-vasc <vde-vasc@student.42.rio>
2022-07-14 19:08:39 -0300

    putting in norm standard

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 04:07:13 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/07/14 13:26:21 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/07/14 18:39:26 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,10 @@
 int	ft_printf(const char *text, ...)
 
 {
-
 	va_list	arguments;
 	int		symbol;
 	int		result;
-	int		count;
 
-	count = 0;
 	symbol = 0;
 	result = 0;
 	va_start(arguments, text);
@@ -29,16 +26,16 @@ int	ft_printf(const char *text, ...)
 	{
 		if (text[symbol] == '%' && ft_strchr("cspdiuxX%", text[symbol + 1]))
 		{
-			result +=  argument_case(text[symbol + 1], arguments);
+			result += argument_case(text[symbol + 1], arguments);
 			symbol++;
 		}
-		else 
+		else
 		{
 			ft_putchar(text[symbol]);
-			count++;
+			result++;
 		}
 		symbol++;
 	}
 	va_end(arguments);
-	return (count + result);
+	return (result);
 }
